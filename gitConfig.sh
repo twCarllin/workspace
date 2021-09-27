c6f75663d27ecc55f113b66e9405db3e115c7c9d
twCarllin <tw.carl.lin@gmail.com>
2021-09-27 10:39:20 +0800

    ADD: git diff tool and acat log format

--- gitConfig.sh
+++ gitConfig.sh
@@ -4,3 +4,8 @@ git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.br branch
 git config --global alias.adog "log --all --decorate --oneline --graph"
+git config --global alias.acat "log --pretty=format:'[%h] %ae, %ar: %s' --stat"
+git config --global alias.d difftool
+
+git config --global diff.tool vimdiff
+git config --global difftool.prompt false
